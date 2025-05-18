028fa45fcaa3b70501a49eb85c0f229b769a7d39
Alex-Majdalani <alexmajdalanimelo@gmail.com>
2025-05-18 12:24:23 -0300

    imagens

--- aula12_Ex/ex015/script.js
+++ aula12_Ex/ex015/script.js
@@ -1,1 +1,11 @@
-alert("Olá mundo!");
+function verificar() {
+  var data = new Date();
+  var ano = data.getFullYear;
+  var fano = window.document.getElementById("inasc");
+  var res = window.document.getElementById("res");
+  if (fano.value.length == 0 || fano.value > ano) {
+    window.alert("[ERRO] Verifique os dados e tente novamente!");
+  } else {
+    window.alert("Tudo OK!");
+  }
+}

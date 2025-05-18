fca3cfd97728cee2056132b3d4a8cb975fbfb93a
Alex-Majdalani <alexmajdalanimelo@gmail.com>
2025-05-18 19:49:27 -0300

    travei no for

---
+++ aula014_Ex/ex016/node_312ebda18d7e3.tmp.js
@@ -0,0 +1,7 @@
+var i = 1;
+var f = 10;
+var p = 2;
+
+for (i; i <= f; i + p) {
+  console.log(i + p);
+}
--- aula014_Ex/ex016/script.js
+++ aula014_Ex/ex016/script.js
@@ -1,1 +1,14 @@
-alert("Olá mundo!");
+function clicar() {
+  var ini = window.document.getElementById("ini");
+  var fim = window.document.getElementById("ifim");
+  var pass = window.document.getElementById("ipass");
+  var res = window.document.getElementById("res");
+  var i = ini.value;
+  var f = fim.value;
+  var p = pass.value;
+
+  for (i; i <= f; i += p) {
+    console.log(i);
+  }
+  res.innerHTML = `A contagem é`;
+}
